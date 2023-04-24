14718ef6f255700ba9a42e4606205532a215dd6c
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-04-24 20:13:04 +0500

    add login register

---
+++ client/pages/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="text-3xl" @click="setLocalstorage">
+      Set LocalStorage
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    setLocalstorage() {
+      if(process.client) {
+        localStorage.setItem('login_id', 'Jerry')
+        this.$router.push('/')
+      }
+    }
+  },
+  created() {
+    if(process.client) {
+      const id = localStorage.getItem('login_id')
+      if(id){
+        this.$router.push('/')
+      }
+    }
+  }
+}
+
+
+</script>
+
+<style>
+
+</style>
